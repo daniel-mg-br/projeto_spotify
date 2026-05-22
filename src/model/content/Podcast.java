eb30561e3535ed42c3dca6399f38a360cea4d381
Daniel M. Garcia <mendoncad38@gmail.com>
2026-05-22 12:58:39 -0300

    Arrumando algumas classes
    Ajustes e padronizações, adição dos diagramas e documento do projeto

--- src/model/content/Podcast.java
+++ src/model/content/Podcast.java
@@ -1,71 +1,81 @@
 package model.content;
-
 import java.util.Date;
 import java.util.List;
 import java.util.ArrayList;
 
-public class Episodio extends Conteudo {
-
-    private int numEpisodio;
-    private Date lancamento;
-    private List<String> convidados;
+// Classe Podcast, representando os podcasts de um Criador, que um Ouvinte pode ouvir
+public class Podcast  {
+	// Atributos privados
+	private int id, numEpisodios;
+	private String nome, tema;
+	private Date criacao;
+	
+	// Lista de episódios do podcast
+	private List <Episodio> episodios;
+	
+	// Métodos Getter e Setter padrão
+    public int getId() {return this.id;}
+    public void setId(int id) {this.id = id;}
     
-    public Episodio() {
-        convidados = new ArrayList<>();
+    public int getNumEpisodios() {return this.numEpisodios;}
+    public void setNumEpisodios(int numEpisodios) {this.numEpisodios = numEpisodios;}
+    
+    public String getNome() {return this.nome;}
+    public void setNome(String nome) {this.nome = nome;}
+    
+    public String getTema() {return this.tema;}
+    public void setTema(String tema) {this.tema = tema;}
+    
+    public Date getCriacao() {return this.criacao;}
+    public void setCriacao(Date criacao) {this.criacao = criacao;}
+    
+    public List <Episodio> getEpisodios() {
+    	return this.episodios;
     }
-
-    public Episodio(int id, String titulo, int duracaoMin,
-                     int numEpisodio, Date lancamento) {
-
-        super(id, titulo, duracaoMin);
-
-        this.numEpisodio = numEpisodio;
-        this.lancamento = lancamento;
-        this.convidados = new ArrayList<>();
+    
+    // Método Construtor
+    public Podcast(int id, String nome, String tema) {
+    	this.id = id;
+    	this.nome = nome;
+    	this.tema = tema;
+    	
+    	this.numEpisodios = 0;
+    	this.setCriacao(new Date());
+    	this.episodios = new ArrayList<>();
     }
-
-    public int getNumEpisodio() {
-        return numEpisodio;
+    
+    // Método para mostrar o título dos episódios do podcast
+    public void mostrarEpisodios() {
+    	for (Episodio e : this.getEpisodios()) {
+    		System.out.println(e.getTitulo());
+    	}
     }
-
-    public void setNumEpisodio(int episodio) {
-        this.numEpisodio = episodio;
+    
+    // Método para adicionar um episódio ao podcast, com verificação
+    public boolean adicionarEp(Episodio e) {
+    	if (this.episodios.contains(e) || e == null) {
+    		return false;
+    	}
+    	this.episodios.add(e);
+    	this.numEpisodios++;
+    	return true;
     }
-
-    public Date getLancamento() {
-        return lancamento;
+    
+    // Método para remover episódios do podcast
+    public boolean removerEp(Episodio e) {
+    	if (this.episodios.remove(e)) {
+    		this.numEpisodios--;
+    		return true;
+    	};
+    	return false;
     }
-
-    public void setLancamento(Date lancamento) {
-        this.lancamento = lancamento;
-    }
-
-    public List<String> getConvidados() {
-        return convidados;
-    }
-
-    public void setConvidados(List<String> convidados) {
-        this.convidados = convidados;
-    }
-
-    @Override
+    
+    // Método para recuperar os dados do podcast
     public String obterDados() {
-        return super.obterDados() +
-               "\nNúmero do episódio: " + numEpisodio +
-               "\nLançamento: " + lancamento;
-    }
-
-    public void mostrarConvidados() {
-        for (String nome : convidados) {
-            System.out.println(nome);
-        }
-    }
-
-    public boolean adicionarConvidado(String nome) {
-        return convidados.add(nome);
-    }
-
-    public boolean removerConvidado(String nome) {
-        return convidados.remove(nome);
+    	return "ID: " + this.getId() + "\n" +
+               "Nome: " + this.getNome() + "\n" +
+    		   "Tema: " + this.getTema() + "\n" +
+               "Episódios: " + this.getNumEpisodios() + "\n" +
+    		   "Criação: " + this.getCriacao();
     }
 }
